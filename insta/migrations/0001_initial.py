01e47304305fbe0110cf0f7342dfc68619c75b82
gwashh <perrysugut75@gmail.com>
2022-06-10 19:58:26 +0300

    add requirement.txt

--- insta/migrations/0001_initial.py
+++ insta/migrations/0001_initial.py
@@ -13,6 +13,7 @@ class Migration(migrations.Migration):
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
+    
 
     operations = [
         migrations.CreateModel(
